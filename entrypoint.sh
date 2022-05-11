1fdac4d5cab7fff55b888e55175f1b429b4df1c5
Avengers PT <647766@protonmail.com>
2022-05-11 11:50:42 +0800

    Remove Hardcoded password, which is set at deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+if [[ -z "$PASS" ]]; then
+    PASS='This!s@+free_te5t~9527'
+fi
+
 exec ss-server \
       -s $SERVER_ADDR \
       -p $SERVER_PORT \
-      -k 'This|s@+free_te5t~9527' \
+      -k ${PASSWORD} \
       -m 'chacha20-ietf-poly1305' \
       -t 300 \
       -d '1.0.0.1' \
