69e996d0118f71055530229ac65fb7219537feb6
Avengers PT <647766@protonmail.com>
2022-05-11 12:09:08 +0800

    Rename from pre-defined name PASS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [[ -z "$PASS" ]]; then
-    PASS='This!s@+free_te5t~9527'
+if [[ -z "$yoursecret251997" ]]; then
+    yoursecret251997='This!s@+free_te5t~9527'
 fi
 
 exec ss-server \
       -s $SERVER_ADDR \
       -p $SERVER_PORT \
-      -k ${PASSWORD} \
+      -k ${yoursecret251997} \
       -m 'chacha20-ietf-poly1305' \
       -t 300 \
       -d '1.0.0.1' \
